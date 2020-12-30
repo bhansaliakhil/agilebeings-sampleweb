2216808962184934712b37790a2d3d211e2a95da
Akhil Bhansali <bhansaliakhil@gmail.com>
2020-12-30 15:49:52 +0530

    change the message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'Akhil, you are live!';
+  const name = process.env.NAME || 'Akhil, you are live today on 30th of December 2020!';
   res.send(`Hello ${name}!`);
 });
 
