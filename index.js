aa5ecd23ebe36669a854c27dc0b59e0e26dbc7cd
Akhil Bhansali <bhansaliakhil@gmail.com>
2020-12-30 17:37:55 +0530

    Adding module Members.js for api testing

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
 
+//importing a json file named Members.js using following command.
+const members = require ('./Members')
+
 const app = express();
+
+//exporting a REST API where this route gets all members. This is used with Postman to check if json is returned.
+app.get("/api/members", (req, res) => res.json(members));
 
 //Set static folder for static websites
 app.use(express.static(path.join(__dirname, "public")));
---
+++ public/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1, 
+        name: 'Akhil Bhansali',
+        email: 'akhil.bhansali@tdtl.world',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Anand Patwa',
+        email: 'anand.patwa@tdtl.world',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Amit Andre',
+        email: 'amit@tdtl.world',
+        status: 'active'
+    }
+];
+
+module.exports = members;
