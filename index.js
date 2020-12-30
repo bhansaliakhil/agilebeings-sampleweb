06690fb02b9603a368cbee713d42c382cd0a5e27
Akhil Bhansali <bhansaliakhil@gmail.com>
2020-12-30 16:20:31 +0530

    stylesheet + static webserver support

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html')); 
-});
+//Set static folder for static websites
+app.use(express.static(path.join(__dirname, "public"));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
